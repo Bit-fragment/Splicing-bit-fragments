1232e568278bb6ac22a3e33654c378107ea1bc34
XMZR <508388068@qq.com>
2022-09-26 08:31:44 +0800

    20220926

--- Main_cpp_modules/TemplatesAndGenericProgramming/ObjektpoolModular/DiyObjektPool.h
+++ Main_cpp_modules/TemplatesAndGenericProgramming/ObjektpoolModular/DiyObjektPool.h
@@ -12,12 +12,22 @@
 template<class T>
 class DiyObjektPool {//设计一个对象池 可以放入任何对象  需要时取出，使用完放回
 private:
-    std::queue<T *> queue;
+    std::queue<T *> queue = nullptr;
+    std::queue<T *> queueName = nullptr;
+    std::initializer_list<T *> list = nullptr;
+    std::queue<int> queueIndex;
+    int size = 10;
+
 public:
     DiyObjektPool<T>() {
-        for (int i = 0; i < 10; i++) {
+        queue = new std::queue<T *>;
+        queueName = new std::queue<T *>;
+        for (int i = 0; i < size; i++) {
             T *t = new T();
             queue.push(t);
+            queueName.push(t);
+            queueIndex.push(0);
+
         }
     };
 
@@ -26,8 +36,9 @@ public:
             T *t = queue.front();
             queue.pop();//内存是否会被释放？
             delete t;
-            t = nullptr;
         }
+        delete queue;
+        queue = nullptr;
     };
 
     T *getObject() {//从池子中获取一个对象
@@ -37,6 +48,16 @@ public:
         return t;//如何自动回收呢？     设计一个引用计数器(类似于智能指针那种效果)
     }
 
+    bool PutObjectBack(T *t) {
+        if (nullptr == t)return false;
+        while (!queue.empty()) {
+            if (queue.front() == t) {
+                return false;
+            }
+        }
+        queue.push(t);
+        return true;
+    }
 };
 
 
