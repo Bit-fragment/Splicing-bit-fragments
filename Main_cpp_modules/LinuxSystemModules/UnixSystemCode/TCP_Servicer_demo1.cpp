857adf99f1f376e3148b004a8372d70a8e5550bf
XMZR <508388068@qq.com>
2022-11-03 15:40:22 +0800

    20221103

--- Main_cpp_modules/LinuxSystemModules/UnixSystemCode/TCP_Servicer_demo1.cpp
+++ Main_cpp_modules/LinuxSystemModules/UnixSystemCode/TCP_Servicer_demo1.cpp
@@ -3,19 +3,12 @@
 //
 
 #include "unp.h"
-#include <time.h>
 
 int main(int argc, char **argv) {
     int listenfd, connfd;
     pid_t childpid;
     socklen_t clilen;
-
-//    socklen_t len;
     struct sockaddr_in servaddr{}, cliaddr{};
-
-//    char buff[MAXLINE];
-//    time_t ticks;
-
     listenfd = Socket(AF_INET, SOCK_STREAM, 0);
 
     bzero(&servaddr, sizeof(servaddr));
@@ -24,22 +17,18 @@ int main(int argc, char **argv) {
     servaddr.sin_port = htons(SERV_PORT);
 
     Bind(listenfd, (SA *) &servaddr, sizeof(servaddr));
-
     Listen(listenfd, LISTENQ);
 
     for (;;) {
-//        len = sizeof(cliaddr);
-//        connfd = Accept(listenfd, (SA *) &cliaddr, &len);
-//        printf("connection from %s,port %d\n",
-//               Inet_ntop(AF_INET, &cliaddr.sin_addr, buff, sizeof(buff),
-//                         ntohs(cliaddr.sin_port)));
-//        ticks = time(NULL);
-//        snprintf(buff, sizeof(buff), "%.24s\r\n", ctime(&ticks));
-//        Write(connfd, buff, strlen(buff));
-
-
+        clilen = sizeof(cliaddr);
+        connfd = Accept(listenfd, (SA *) &cliaddr, &clilen);
+        if ((childpid = Fork()) == 0) {
+            Close(listenfd);
+            str_echo(connfd);
+            exit(0);
+        }
+        Close(connfd);
 
         Close(connfd);
     }
-    return 0;
 }
