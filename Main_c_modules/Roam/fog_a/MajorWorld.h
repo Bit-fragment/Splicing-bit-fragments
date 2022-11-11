0ae451eb6039778cd7c7c8b5f5bc9d6035d376c9
XMZR <508388068@qq.com>
2022-11-11 14:39:37 +0800

    2022 11 11

---
+++ Main_c_modules/Roam/fog_a/MajorWorld.h
@@ -0,0 +1,13 @@
+//
+// Created by 20106 on 2022/11/4.
+//
+
+#ifndef STRUCTURALMODE_MAJORWORLD_H
+#define STRUCTURALMODE_MAJORWORLD_H
+
+int f1(int x, int y) {
+    return x + y;
+}
+
+
+#endif //STRUCTURALMODE_MAJORWORLD_H
---
+++ Main_c_modules/Roam/main.c
@@ -0,0 +1,12 @@
+//
+// Created by 20106 on 2022/11/4.
+//
+#define OUTUPT_FORMATS system("chcp 65001");
+
+#include <stdio.h>
+#include "fog_a/MajorWorld.h"
+
+int main() {
+    printf("%d \n", f1(100, 20));
+    return 0;
+}
