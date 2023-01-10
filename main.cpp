ad0120a9888690ca9c2371a86c9c1f34d414be12
XMZR <508388068@qq.com>
2023-01-10 10:40:07 +0800

    2023 01 10

--- main.cpp
+++ main.cpp
@@ -2,9 +2,7 @@
 #include <vector>
 #include <string>
 #include <thread>
-#include <array>
 #include <cstring>
-#include <fstream>
 #include "windows.h"
 
 using namespace std;
@@ -13,21 +11,12 @@ void Hello() {
     std::cout << "hello world, I'm a thread!" << std::endl;
 }
 
-auto getList1(int size) {
-    return vector<int>(size);
-}
-
-int *getList2(int size) {
-    int *pi = new int[size];
-    return pi;
-}
-
 struct Ob {
 public:
     string email;
     string password;
 
-    string toString() {
+    string toString() const {
         string str;
         str += "{email:" + email + ",password:" + password + "}";
         return str;
@@ -41,7 +30,7 @@ public:
     double yue = 0;
     Ob ob;
 
-    string toString() {
+    string toString() const {
         string str;
         str += "{age:" + to_string(age) + ",name:" + name + ",yue:" + to_string(yue) + ",ob:" + ob.toString() + "}";
         return str;
@@ -60,16 +49,11 @@ int *getMyList(int size) {
     return pi;
 }
 
-template<class T>
-void wenhao(T value) {
-    cout << typeid(value).name() << endl;
-}
-
 
 int main() {
 
-/*    int a = 1, b = -1;
-    while (1) {
+    int a = 1, b = -1, c = 0;
+    while (c < 10) {
         Sleep(1000);
         try {
             if (a + b == 0)throw "Denominator cannot be zero Exception ";
@@ -79,9 +63,12 @@ int main() {
             cout << p << b << endl;
             b < 0 ? b = 1 : b = -1;
         }
-    }*/
+        c++;
+    }
 
-    /*int qwe[5];//数组与指针的关系
+    /*======================================================================*/
+
+    int qwe[5];//数组与指针的关系
     int *pio = qwe;
     for (int i = 0; i < 5; ++i) {
         cout << &pio[i] << " ";
@@ -92,6 +79,8 @@ int main() {
     cout << "pio: " << pio << endl;
     //cout << "*pio: " << *pio << endl;
     cout << "&pio: " << &pio << endl;
+
+    /*======================================================================*/
 
     cout << endl;
     int *pp = getMyList(5);
@@ -106,9 +95,11 @@ int main() {
     }
 
     delete[]pp;
-    delete[]po;*/
+    delete[]po;
 
-    /*char char_str[] = "wenhao";
+    /*======================================================================*/
+
+    char char_str[] = "wenhao";
     cout << "\nchar_str:" << char_str << ";size:" << strlen(char_str) << endl;
     char *pkl = new char[strlen(char_str) + 1];
     //strcpy()函数接受2个参数。第1个是目标地址，第2个是要复制的 字符串的地址。
@@ -120,16 +111,20 @@ int main() {
     for (char item: char_str) {
         cout << item;
     }
-    delete[] pkl;*/
+    delete[] pkl;
 
-    /*Ob *obpi = new Ob{"wenhao", "password"};
+    /*======================================================================*/
+
+    Ob *obpi = new Ob{"wenhao", "password"};
     Ob *obpi1 = new Ob{"1", "2"};
     cout << obpi->email << " " << obpi->password << " obpi:" << &obpi << endl;
     cout << (*obpi1).email << " " << (*obpi1).password << " obpi1:" << &obpi1 << endl;
     delete obpi1;
-    delete obpi;*/
+    delete obpi;
 
-    /*tuple<string, vector<int>, int> mydata{"wenhao", {1, 4}, 6};
+    /*======================================================================*/
+
+    tuple<string, vector<int>, int> mydata{"wenhao", {1, 4}, 6};
     auto p1 = get<0>(mydata);
     auto p2 = get<1>(mydata);
     auto p3 = get<2>(mydata);
@@ -139,22 +134,26 @@ int main() {
         cout << item << " ";
     }
     cout << endl;
-    cout << p3 << endl;*/
+    cout << p3 << endl;
 
-    /*char char_str11[5];
+    /*======================================================================*/
+
+    char char_str11[5];
     cout << "Enter:" << endl;
     cin >> char_str11;
     char *char_list2 = new char[strlen(char_str11) + 1];
     strcpy(char_list2, char_str11);
     cout << "char_list2: " << char_list2 << endl;
-    delete[]char_list2;*/
+    delete[]char_list2;
 
-    /*User list[10];//枚举测试
+    /*======================================================================*/
+
+    User list[10];//枚举测试
     for (int i = 0; i < 10; ++i) {
         list[i] = {10 + i, "wocao" + to_string(i), 123.123987 + i, {"201067242@qq.com", "42901002wen"}};
     }
-    for (int i = 0; i < 10; ++i) {
-        cout << list[i].toString() << endl;
+    for (const auto &i: list) {
+        cout << i.toString() << endl;
     }
     vector<User> userList;
     for (int i = 0; i < 10; ++i) {
@@ -176,81 +175,17 @@ int main() {
     int *pi = list3[0];
     for (int i = 0; i < 6; ++i) {
         cout << pi[i] << " ";
-    }*/
-
-    /*User user2 = {20, "wocao", 123.123987, {"201067242@qq.com", "42901002wen"}};
-    cout << user2.toString() << endl;*/
-
-    /*vector<int> list1 = getList1(4);
-    int size = 10;
-    int *pi = getList2(size);
-    for (int i = 0; i < size; ++i) {
-        pi[i] = i;
-        cout << pi[i] << " ";
     }
-    delete pi;
-    cout << "\n" << list1.size() << endl;*/
 
-    /*vector<int> list11 = {1, 2};
-    cout << list11.begin().base() << endl;
-    cout << list11.end().base() << endl;
-    int *po = list11.begin().base();
-    cout << *po << endl;
-    cout << list11.end() - list11.begin() << endl;
-    cout << (int *) "hello" << endl;*/
+    /*======================================================================*/
 
-    /*Object ob1;
-    ob1.run(1, 2);*/
+    User user2 = {20, "wocao", 123.123987, {"201067242@qq.com", "qwe123123qwe"}};
+    cout << user2.toString() << endl;
 
-    /*Object *pp = new Object;
-    cout << "pp->f2(100): " << pp->f2(100) << endl;
-    delete pp;
-    std::shared_ptr<Object> pi = make_shared<Object>();
-    cout << "pi->f2(100): " << pi->f2(100) << endl*/;
+    /*======================================================================*/
 
-    /*thread thrd(&Hello);
-    thrd.join();*/
-
-    /*vector<int> list = {10, 2, 3, 41, 23, 6};
-    for (const auto &item: list) {
-        cout << item << " ";
-    }
-    cout << endl;
-
-    vector<int> list1 = object.f1(list);
-    for (const auto &item: list1) {
-        cout << item << " ";
-    }
-    cout << endl;
-    std::cout << "Hello, World!" << std::endl;
-    cout << object.f2(100) << endl;
-
-    string str = "goodBey";
-    cout << "body:" << str << " size():" << str.size() << endl;
-
-    string bodyValue = "Hello world";
-    cout << "bodyValue:" << bodyValue << endl;
-    cout << "bodyValue(*):" << object.f3(bodyValue) << endl;
-
-    vector<string> charList = {};
-    for (int i = 0; i < 10; ++i) {
-        charList.push_back(to_string(i));
-    }
-    cout << charList.size() << endl;
-    for (const auto &item: charList)
-        cout << item << " ";
-    cout << endl;*/
-
-    /*vector<std::string> list2 = user.readFileBody(R"(C:\Users\XMZR\Desktop\test.txt)");
-    for (const auto &item: list2)
-        cout << item;
-    cout << endl;*/
-
-    /*for (int i = 0; i < 10; ++i) {
-        string str = "wenhao";
-        str += to_string(i) += "\n";//字符串类型拼接
-        user1.addFileBody(R"(C:\Users\XMZR\Desktop\wenhao.txt)", str);
-    }*/
+    thread thrd(&Hello);
+    thrd.join();
 
     return 0;
 }
