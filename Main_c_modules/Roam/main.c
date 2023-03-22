968114bf5212ecc0965f3da1286ee65801da9959
XMZR <508388068@qq.com>
2023-03-22 15:54:58 +0800

    20230322

--- Main_c_modules/Roam/main.c
+++ Main_c_modules/Roam/main.c
@@ -1,29 +1,35 @@
-//
-// Created by 20106 on 2022/11/11.
-//
-#define OUTUPT_FORMATS system("chcp 65001");
-
+#include <stdlib.h>
 #include <stdio.h>
-#include "fog_a/MethodeList.h"
-#include <ctype.h>
-#include <locale.h>
 
-int main() {
-    printf("%d \n", f1(100, 20));
+struct data {
+    int nr;
+    char const *value;
+} dat[] = {
+        {1, "Foo"},
+        {2, "Bar"},
+        {3, "Hello"},
+        {4, "World"}
+};
 
-//    printf("%d\n",isalnum('a'));
-//    printf("%d\n",isalnum('1'));
-//    printf("%d\n",isalnum(','));
-//    printf("%d\n",isalnum(' '));
+int data_cmp(void const *lhs, void const *rhs) {
+    struct data const *const l = lhs;
+    struct data const *const r = rhs;
 
-    printf("%c\n", tolower('R'));
-    printf("%c\n", toupper('a'));
+    if (l->nr < r->nr) return -1;
+    else if (l->nr > r->nr) return 1;
+    else return 0;
 
-    printf("%d\n", islower('a'));
-    printf("%d\n", islower('A'));
+    // return (l->nr > r->nr) - (l->nr < r->nr); // 可行的简洁写法
+    // return l->nr - r->nr; // 错误的简洁写法（若给出 INT_MIN 就会失败）
+}
 
-    printf("%s\n", setlocale(0, ""));
-
-
-    return 0;
+int main(void) {
+    struct data key = {.nr = 3};
+    struct data const *res = bsearch(&key, dat, sizeof dat / sizeof dat[0],
+                                     sizeof dat[0], data_cmp);
+    if (res) {
+        printf("No %d: %s\n", res->nr, res->value);
+    } else {
+        printf("No %d not found\n", key.nr);
+    }
 }
